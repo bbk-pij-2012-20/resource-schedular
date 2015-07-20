5dceb27d6c79f8ae5d1dae1c78ea9c654c27c9b9
shahin.zibaee <shahin.zibaee"ingg.com>
2015-07-20 16:50:41 +0100

    added first test class skeleton

---
+++ src/test/java/com/jpm/ipb/SchedulerTest.java
@@ -0,0 +1,30 @@
+package com.jpm.ipb;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by shahin.zibaee on 20/07/2015.
+ */
+public class SchedulerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testHasMessagesToProcess() throws Exception {
+
+
+
+    }
+
+    public void testSchedule() throws Exception {
+
+        
+
+    }
+}
