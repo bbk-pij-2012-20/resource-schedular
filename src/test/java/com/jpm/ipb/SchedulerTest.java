c8c8b336d7fd6acabfa9dddab4b41e777a317c50
shahin.zibaee <shahin.zibaee"ingg.com>
2015-07-21 13:08:40 +0100

    first Mockito test written, passes - testHasMessagesToProcess()

--- src/test/java/com/jpm/ipb/SchedulerTest.java
+++ src/test/java/com/jpm/ipb/SchedulerTest.java
@@ -1,30 +1,52 @@
 package com.jpm.ipb;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-/**
- * Created by shahin.zibaee on 20/07/2015.
- */
-public class SchedulerTest extends TestCase {
+import static org.mockito.Mockito.*;
+import java.util.LinkedList;
 
+@RunWith(MockitoJUnitRunner.class)
+public class SchedulerTest {
+
+    @Mock
+    private Message mockFirstMsg;//using annotation to create a mock object rather than using mock()
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+
+
 
     }
 
+    @After
     public void tearDown() throws Exception {
 
+
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
     public void testHasMessagesToProcess() throws Exception {
 
+        MockitoAnnotations.initMocks(mockFirstMsg);//using annotation to create a mock object rather than using mock()
+        Message mockFirstMsg =  mock(MessageGroup1.class);//using mock() to create a mock object
+        LinkedList<Message> mockLinkedList = mock(LinkedList.class);//mocking a concrete linked list
+        when(mockLinkedList.get(0)).thenReturn(mockFirstMsg);//stubbing
 
 
     }
 
+    @Test
     public void testSchedule() throws Exception {
 
-        
+
 
     }
 }
